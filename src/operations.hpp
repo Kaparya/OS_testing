cee683ab583480fb252a55ecac1851762e9c1949
Damir Kandelov <kaparyaka@mail.ru>
2024-06-12 01:36:06 +0300

    Style fix

--- src/operations.hpp
+++ src/operations.hpp
@@ -1,23 +1,23 @@
 #ifndef SUM_H
 #define SUM_H
 
-template<class T>
-T sum(const T &lhs, const T &rhs) {
+template <class T>
+T sum(const T& lhs, const T& rhs) {
     return lhs + rhs;
 }
 
-template<class T>
-T subtraction(const T &lhs, const T &rhs) {
+template <class T>
+T subtraction(const T& lhs, const T& rhs) {
     return lhs - rhs;
 }
 
-template<class T>
-T multiplication(const T &lhs, const T &rhs) {
+template <class T>
+T multiplication(const T& lhs, const T& rhs) {
     return lhs * rhs;
 }
 
-template<class T>
-T division(const T &lhs, const T &rhs) {
+template <class T>
+T division(const T& lhs, const T& rhs) {
     return lhs / rhs;
 }
 
