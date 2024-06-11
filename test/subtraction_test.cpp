a9ea81566918a4b231f2e42f1c2f71e047b30d69
Damir Kandelov <kaparyaka@mail.ru>
2024-06-12 01:00:20 +0300

    Fix failed test

--- test/subtraction_test.cpp
+++ test/subtraction_test.cpp
@@ -3,7 +3,7 @@
 #include "../src/operations.hpp"
 
 TEST(OperationsTest, Subtraction) {
-    EXPECT_EQ(subtraction(3, 1), 3);
+    EXPECT_EQ(subtraction(3, 1), 2);
     EXPECT_FLOAT_EQ(subtraction(1.2f, -1.2f), 2.4f);
     EXPECT_FLOAT_EQ(subtraction(1e9, 3e9), -2e9);
 }
